07892f46485157d44d9b565bf4e50e70a2d8b02b
VAIBHAV-25 <vaibhavsinghvi254@gmail.com>
2022-06-15 10:50:54 +0530

    first commit

---
+++ src/components/HeroBanner.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Box, Stack, Typography } from '@mui/material';
+
+import HeroBannerImage from '../assets/images/banner.png';
+
+const HeroBanner = () => (
+  <Box sx={{ mt: { lg: '212px', xs: '70px' }, ml: { sm: '50px' } }} position="relative" p="20px">
+    <Typography color="#0583a0" fontWeight="600" fontSize="26px">Welcome to Fitpro.com</Typography>
+    <Typography fontWeight={700} sx={{ fontSize: { lg: '44px', xs: '40px' } }} mb="23px" mt="30px">
+      The healthy way of
+      <br />
+      life company
+    </Typography>
+    <Typography fontSize="22px" lineHeight="35px">
+      Check out the most effective exercises personalized to you
+    </Typography>
+    <Stack>
+      <a href="#exercises" style={{ marginTop: '45px', textDecoration: 'none', width: '200px', textAlign: 'center', background: '#0583a0', padding: '14px', fontSize: '22px', textTransform: 'none', color: 'white', borderRadius: '4px' }}>Explore Exercises</a>
+    </Stack>
+    <Typography fontWeight={600} color="#0583a0" sx={{ opacity: '0.1', display: { lg: 'block', xs: 'none' }, fontSize: '200px' }}>
+      Exercise
+    </Typography>
+    <img src={HeroBannerImage} alt="hero-banner" className="hero-banner-img" />
+  </Box>
+);
+
+export default HeroBanner;
